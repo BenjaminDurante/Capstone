75a6e94ce3586b1c7f23f95173841e3587bab69c
BenjaminDurante <61442696+BenjaminDurante@users.noreply.github.com>
2020-02-26 15:14:29 -0700

    Altered Pressure class
    Removed the setup function and incorporated port assignment directly into the run function so that multiple pressure transducers can be run at the same time from the same class.

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -1,17 +1,16 @@
-
 #include "Pot.h"
 #include "Printer.h"
 #include "Pressure.h"
 
+char pot = A1;
+char pressure1 = A0;
+char pressure2 = A2;
+int baud = 9600;
 
 void setup() {
   // put your setup code here, to run once:
-  char pot = A1;
-  char pressure = A0;
-  int baud = 9600;
 
 
-  Pressure.Pressure_Setup(pressure);
   Pot.Pot_Setup(pot);
 
   Serial.begin(baud);
@@ -20,7 +19,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.print(Pot.Pot_Run()); Serial.print(" , "); Serial.print(Pressure.Pressure_Run()); Serial.println("PSI");
+  Serial.print(Pot.Pot_Run()); Serial.print(" , P1:"); Serial.print(Pressure.Pressure_Run(pressure1)); Serial.print("PSI, P2:"); Serial.print(Pressure.Pressure_Run(pressure2)); Serial.println("PSI");
   delay(100);        // delay in between reads for stability
 
 }
--- Main_Code/Pressure.h
+++ Main_Code/Pressure.h
@@ -1,29 +1,23 @@
 class Pressure {
   private:
 
-  //Class Variables
-  char _trigPin;
-  float _Product;
+    //Class Variables
+    float _Product;
 
   public:
 
-//Defines which port the Pressure Transducer is connected to
-  void Pressure_Setup(char trigPin){
-    _trigPin = trigPin;
-  }
+    //Reads the Pressure Transducer value and outputs the pressure
+    float Pressure_Run(char trigPin) {
+      _Product = analogRead(trigPin);
+      /*
 
-//Reads the Pressure Transducer value and outputs the pressure 
-  float Pressure_Run() {
-    _Product = analogRead(_trigPin);
-    /*
-     * 
-     * 
-     * Voltage to Pressure conversion below
-     * 
-     * 
-     */
-    _Product = _Product/1024*5*30; //Converts voltage to pressure in PSI
-    return _Product; //Outputs pressure in PSI
-  }
+
+         Voltage to Pressure conversion below
+
+
+      */
+      _Product = _Product / 1024 * 5 * 30; //Converts voltage to pressure in PSI
+      return _Product; //Outputs pressure in PSI
+    }
 };
 Pressure Pressure;
