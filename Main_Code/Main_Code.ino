ab47418f11f68a7ae0303529114997d6049f3303
BenjaminDurante <61442696+BenjaminDurante@users.noreply.github.com>
2020-02-27 09:30:14 -0700

    Performance Achieved
    Successfully able to read force data, however accuracy of data is questionable.

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -1,25 +1,28 @@
 #include "Pot.h"
 #include "Printer.h"
 #include "Pressure.h"
+#include "LoadCell.h"
 
 char pot = A1;
 char pressure1 = A0;
 char pressure2 = A2;
 int baud = 9600;
+int LOADCELL_DOUT_PIN = 3;
+int LOADCELL_SCK_PIN = 2;
 
 void setup() {
   // put your setup code here, to run once:
 
 
   Pot.Pot_Setup(pot);
-
+LoadCell.LoadCell_Setup(LOADCELL_DOUT_PIN,LOADCELL_SCK_PIN);
   Serial.begin(baud);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.print(Pot.Pot_Run()); Serial.print(" , P1:"); Serial.print(Pressure.Pressure_Run(pressure1)); Serial.print("PSI, P2:"); Serial.print(Pressure.Pressure_Run(pressure2)); Serial.println("PSI");
+  Serial.print(Pot.Pot_Run()); Serial.print(" , P1:"); Serial.print(Pressure.Pressure_Run(pressure1)); Serial.print("PSI, P2:"); Serial.print(Pressure.Pressure_Run(pressure2)); Serial.print("PSI, Force:"); Serial.print(LoadCell.LoadCell_Run()); Serial.println("lbs");
   delay(100);        // delay in between reads for stability
 
 }
