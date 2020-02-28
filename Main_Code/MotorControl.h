598cf3c307290727c690fae6b5d980e2df0d3dc5
ConnorBuchholz <61481050+ConnorBuchholz@users.noreply.github.com>
2020-02-28 15:41:06 -0700

    Motor Controller Implemented
    Changed MotorController class to have middle zone where actuator does not move. Commented on Motor controller class. Implemented the MotorControl Class to the main code with commenting.

--- Main_Code/MotorControl.h
+++ Main_Code/MotorControl.h
@@ -9,27 +9,35 @@ class MotorControl
 
   public:
 
+  //Sets up the pins for output
   void PWMSetup (int pwm, int dir)
   {
     pinMode(pwm,OUTPUT);
     pinMode(dir,OUTPUT);
   }
 
-  void MotorMove(int pos, int PotValue)
+  void MotorMove(int pos, int PotValue, int dir, int pwm)//PotValue is the target value and will change with the control system, pos is the current position
   {
-    if(PotValue>pos)
+    if(PotValue>pos)//if target is greater than current position then extend
     {
-      pwm_value = 255;
-        digitalWrite(dir,LOW);
-        analogWrite(pwm,pwm_value);
+      pwm_value = 255;//speed
+        digitalWrite(dir,HIGH);//writing to motor controller direction of travel
+        analogWrite(pwm,pwm_value);//writing to motor controller speed of travel
+    }
+
+    if(PotValue==pos)//if target is at current position sets speed to 0, may need to convert to a range in the future so that small impulses dont affect as much
+    {
+      pwm_value = 0;
+        digitalWrite(dir,HIGH);//writing to motor controller direction of travel, direction doesnt matter in this case since speed is zero
+        analogWrite(pwm,pwm_value);//writing to motor controller speed of travel
     }
     
-    if(PotValue<pos)
+    if(PotValue<pos)//if target value is less than current position then retract
     {
       pwm_value = 255;
-        digitalWrite(dir,HIGH);
-        analogWrite(pwm,pwm_value);
+        digitalWrite(dir,LOW);//writing to motor controller direction of travel
+        analogWrite(pwm,pwm_value);//writing to motor controller speed of travel
     }
   }
-}
+};
 MotorControl MotorControl;
