4371e302008c0e5639d3d1fa06510e291fbab1a2
BenjaminDurante <61442696+BenjaminDurante@users.noreply.github.com>
2020-02-27 19:52:28 -0700

    Prepped main function for multiple input types
    Added variables for implementation later.

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -2,9 +2,14 @@
 #include "Printer.h"
 #include "Pressure.h"
 
-char pot = A1;
+char pot1 = A1;
+char pot2;
 char pressure1 = A0;
 char pressure2 = A2;
+int motordir1;
+int motorpwm1;
+int motordir2;
+int motorpwm2;
 int baud = 9600;
 
 void setup() {
@@ -16,7 +21,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.print(Pot.Pot_Run(pot)); Serial.print(" , P1:"); Serial.print(Pressure.Pressure_Run(pressure1)); Serial.print("PSI, P2:"); Serial.print(Pressure.Pressure_Run(pressure2)); Serial.println("PSI");
+  Serial.print(Pot.Pot_Run(pot1)); Serial.print(" , P1:"); Serial.print(Pressure.Pressure_Run(pressure1)); Serial.print("PSI, P2:"); Serial.print(Pressure.Pressure_Run(pressure2)); Serial.println("PSI");
   delay(100);        // delay in between reads for stability
 
 }
