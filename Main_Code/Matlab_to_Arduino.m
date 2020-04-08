fccfaf8c37636fcabcdd1666533d8e7beffcf418
BenjaminDurante <61442696+BenjaminDurante@users.noreply.github.com>
2020-04-08 10:46:23 -0600

    Adding Comments
    Adding additional comments to the MATLAB script which transfers excel speed data to the Arduino.

--- Main_Code/Matlab_to_Arduino.m
+++ Main_Code/Matlab_to_Arduino.m
@@ -1,17 +1,23 @@
+%Matlab to Arduino data upload
+%Any questions please contact benjamin.durante@ucalgary.ca
 clear 
 clc
 
 %Uploads Excel Sheet that has been preformatted
 T = readtable('Comp_Data_autocross.xlsx');
 %stackedplot(T);
-A = table2array(T);
+A = table2array(T); %Converts the table to an array
 
 % Code to read and upload vehicle speed to the Arduino
-ar = serial('COM4','BaudRate',9600);
+ar = serial('COM4','BaudRate',9600); 
+%May need to change the COM port depending on your computer
+
+%Begins the transfer to the Arduino, one element at a time
 fopen(ar);
 for (i=1:length(A))
     A(i,2)
     fprintf(ar,'%s',char(A(i,2)));
-    pause(20/1000);
+    pause(20/1000); %Data rate in seconds
 end
 fclose(ar);
+%Ends the transfer of data to the Arduino
